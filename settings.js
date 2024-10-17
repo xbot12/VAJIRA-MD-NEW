f13c3d83b395ee3bf753b57a79cc4836b8ec182b
xbot12 <schamith17@gmail.com>
2024-10-17 18:32:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+VAJIRA-MD=7U00mQSb#IJVvHcARjDTQ1OY30HOdvhwUbnDHOgIYSCvy_ycPFTY,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
